a869f9c7536c617a70bea098533782de73f4f877
Mahesh <maheshkoilapu8@gmail.com>
2025-12-01 11:04:35 +0530

    Heart Beat Backend

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://heartbeat-backend-l8pt.onrender.com/api',
   withCredentials: true,
 });
 
